140a0357f2d6bef17de0ec82ce31801ce3707863
Balaji-Karnan <balajirocky321@gmail.com>
2018-09-24 09:22:31 +0530

    first commit

---
+++ src/main/java/com/ibm/javaspring/springcurdangular/model/User.java
@@ -0,0 +1,51 @@
+package com.ibm.javaspring.springcurdangular.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="userss")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String email;
+	private String department;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getemail() {
+		return email;
+	}
+	public void setemail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public User() {
+		
+	}
+	
+}
